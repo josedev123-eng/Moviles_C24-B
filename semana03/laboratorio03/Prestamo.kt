72c75f6b37bcef9739864f60924e8c5d087d1174
Jose Rojas <maxrojasrc887@gmail.com>
2026-08-31 11:31:02 -0500

    Implementacion de calculo de retraso y tarifas en Prestamo

--- semana03/laboratorio03/Prestamo.kt
+++ semana03/laboratorio03/Prestamo.kt
@@ -1,9 +1,30 @@
 package semana03.laboratorio03
+import java.time.LocalDate
 
 class Prestamo(
-    val idPrestamo: String,
-    val nombreCliente: String,
-    val montoPrestado: Double,
-    val diasPactados: Int,
-    val diasReales: Int
-)
+    val tituloLibro: String,
+    val tipoUsuario: String,
+    val fechaPrestamo: LocalDate,
+    val fechaEntregaEsperada: LocalDate,
+    val fechaDevolucionReal: LocalDate
+) {
+    val tarifaPorDia: Double = when (tipoUsuario.lowercase()) {
+        "docente" -> 3.0
+        "alumno" -> 1.50
+        else -> 0
+    }
+
+    fun obtenerDiasRetraso(): Long {
+        val dias = java.time.temporal.ChronoUnit.DAYS.between(fechaEntregaEsperada, fechaDevolucionReal)
+        return if (dias > 0) dias else 0
+    }
+
+    fun obtenerEstado(): String {
+        val dias = obtenerDiasRetraso()
+        return if (dias > 0) {
+            "Devuelto con $dias días de retraso"
+        } else {
+            "Entregado a tiempo"
+        }
+    }
+}
