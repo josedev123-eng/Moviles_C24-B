5af7ffed4044dc68205fe1c2388180fece35e8ec
Jose Rojas <maxrojasrc887@gmail.com>
2026-08-31 11:05:49 -0500

    Crear clase Prestamo con sus atributos principales

---
+++ semana03/laboratorio03/Prestamo.kt
@@ -0,0 +1,9 @@
+package semana03.laboratorio03
+
+class Prestamo(
+    val idPrestamo: String,
+    val nombreCliente: String,
+    val montoPrestado: Double,
+    val diasPactados: Int,
+    val diasReales: Int
+)
