e29655a535dbab586f7a51e1e5d09bb6a3c6c2c5
Jose Rojas <maxrojasrc887@gmail.com>
2026-09-02 03:29:00 -0500

    Proyecto inicial con menu del carrito

---
+++ semana02/laboratorio02/Lab02CarritoKotlin/Carrito.kt
@@ -0,0 +1,7 @@
+package semana02.laboratorio02.Lab02CarritoKotlin
+
+fun main() {
+    println("=========================================")
+    println(" CARRITO DE COMPRAS- TIENDA TECSUP ")
+    println("=========================================")
+}
