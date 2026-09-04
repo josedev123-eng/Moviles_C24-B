4adf2fd0e0baa9b53cb42981387bc8ef3bfa1b6b
Jose Rojas <maxrojasrc887@gmail.com>
2026-09-03 21:43:26 -0500

    Se agrego paquete, imports y paleta de colores

---
+++ semana03/laboratorio03-2/Lab03RegistroProducto/app/src/main/java/com/rojas/lab03registroproducto/RegistroNotasScreen.kt
@@ -0,0 +1,29 @@
+package com.rojas.lab03registroproducto
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import kotlin.math.roundToInt
+
+val PrimaryPurple = Color(0xFF6750A4)
+val DarkPurple = Color(0xFF4A3780)
+val LightBackgroundStart = Color(0xFFEADDFF)
+val LightBackgroundEnd = Color(0xFFF3EDF7)
+val BadgeBgColor = Color(0xFFE8DEF8)
+
+val GreenDark = Color(0xFF1B5E20)
+val Amber = Color(0xFFFFB300)
+val Red = Color(0xFFE53935)
+
