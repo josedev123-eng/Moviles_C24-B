d0799f3db94b08ec98403954d10cb32e308156e6
Jose Rojas <maxrojasrc887@gmail.com>
2026-09-03 23:36:48 -0500

    Integra RegistroNotasScreen en MainActivity

--- semana03/laboratorio03-2/Lab03RegistroProducto/app/src/main/java/com/rojas/lab03registroproducto/MainActivity.kt
+++ semana03/laboratorio03-2/Lab03RegistroProducto/app/src/main/java/com/rojas/lab03registroproducto/MainActivity.kt
@@ -19,29 +19,8 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             Lab03RegistroProductoTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                RegistroNotasScreen()
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    Lab03RegistroProductoTheme {
-        Greeting("Android")
     }
 }
