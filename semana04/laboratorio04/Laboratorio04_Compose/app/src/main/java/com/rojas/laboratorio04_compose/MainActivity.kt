66b6d713f58b1eca1731a53a489f4b635b4bc313
Jose Rojas <maxrojasrc887@gmail.com>
2026-09-16 06:55:23 -0500

    Se implemente el composable TemperatureDisplay

---
+++ semana04/laboratorio04/Laboratorio04_Compose/app/src/main/java/com/rojas/laboratorio04_compose/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.rojas.laboratorio04_compose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.rojas.laboratorio04_compose.ui.theme.Laboratorio04_ComposeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Laboratorio04_ComposeTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Laboratorio04_ComposeTheme {
+        Greeting("Android")
+    }
+}
