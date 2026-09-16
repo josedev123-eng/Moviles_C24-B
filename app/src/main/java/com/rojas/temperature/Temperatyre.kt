180d8b891a9613199a9d17e961e697019c1400fe
Jose Rojas <maxrojasrc887@gmail.com>
2026-09-16 15:03:49 -0500

    Crea un nuevo composable llamado TemperatureDispla

---
+++ app/src/main/java/com/rojas/temperature/Temperatyre.kt
@@ -0,0 +1,11 @@
+package com.rojas.laboratorio04_compose
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun TemperatureDisplay() {}
