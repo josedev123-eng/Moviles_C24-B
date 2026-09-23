62fa69315bd629a01977938f5ae33a5cae4381a8
Jose Rojas <maxrojasrc887@gmail.com>
2026-09-23 06:29:08 -0500

    Configurar el punto de entrada de la app que carga el mapa de navegacion al abrir, muestra primero la pantalla de inicio y extiende el contenido a toda la pantalla del celular

--- semana06/laboratorio06/NavLab/app/src/main/java/com/rojas/navlab/MainActivity.kt
+++ semana06/laboratorio06/NavLab/app/src/main/java/com/rojas/navlab/MainActivity.kt
@@ -4,13 +4,19 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import com.rojas.navlab.navigation.AppNavigation
+import com.rojas.navlab.navigation.Screen
+import com.rojas.navlab.screens.DetailScreen
+import com.rojas.navlab.screens.HomeScreen
+import com.rojas.navlab.screens.ListScreen
+import com.rojas.navlab.screens.ProfileScreen
 import com.rojas.navlab.ui.theme.NavLabTheme
 
 class MainActivity : ComponentActivity() {
@@ -18,30 +24,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            NavLabTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+            AppNavigation()
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    NavLabTheme {
-        Greeting("Android")
     }
 }
